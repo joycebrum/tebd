0c195e7a189d5f24b4ccd4c0fe75a5ad6596eb0e
Joyce Brum <joycebrumu.u@gmail.com>
2019-11-24 21:51:08 -0300

    apanhando para fazer o gafo 2

--- src/app/graph.ts
+++ src/app/graph.ts
@@ -2,82 +2,12 @@
 import * as d3 from 'd3'
 
 export class Graph {
-    drag = simulation => {
-
-        function dragstarted(d) {
-            if (!d3.event.active) simulation.alphaTarget(0.3).restart();
-            d.fx = d.x;
-            d.fy = d.y;
-        }
-        
-        function dragged(d) {
-            d.fx = d3.event.x;
-            d.fy = d3.event.y;
-        }
-        
-        function dragended(d) {
-            if (!d3.event.active) simulation.alphaTarget(0);
-            d.fx = null;
-            d.fy = null;
-        }
-        
-        return d3.drag()
-            .on("start", dragstarted)
-            .on("drag", dragged)
-            .on("end", dragended);
-    }
+    
     constructor(private data, private width, private height) {
-        const color = () => {
-            scale : d3.scaleOrdinal(d3.schemeCategory10);
-            return d => scale(d.group);
-          }
-        const links = this.data.links.map(d => Object.create(d));
-        const nodes = this.data.nodes.map(d => Object.create(d));
-      
-        const simulation = d3.forceSimulation(nodes)
-            .force("link", d3.forceLink(links).id(d => d.id))
-            .force("charge", d3.forceManyBody())
-            .force("center", d3.forceCenter(this.width / 2, this.height / 2));
-      
-        const svg = d3.create("svg")
-            .attr("viewBox", [0, 0, this.width, this.height]);
-      
-        const link = svg.append("g")
-            .attr("stroke", "#999")
-            .attr("stroke-opacity", 0.6)
-          .selectAll("line")
-          .data(links)
-          .join("line")
-            .attr("stroke-width", d => Math.sqrt(d.value));
-      
-        const node = svg.append("g")
-            .attr("stroke", "#fff")
-            .attr("stroke-width", 1.5)
-          .selectAll("circle")
-          .data(nodes)
-          .join("circle")
-            .attr("r", 5)
-            .attr("fill", color)
-            .call(this.drag(simulation));
-      
-        node.append("title")
-            .text(d => d.id);
-      
-        simulation.on("tick", () => {
-          link
-              .attr("x1", d => d.source.x)
-              .attr("y1", d => d.source.y)
-              .attr("x2", d => d.target.x)
-              .attr("y2", d => d.target.y);
-      
-          node
-              .attr("cx", d => d.x)
-              .attr("cy", d => d.y);
-        });
-      
-        //invalidation.then(() => simulation.stop());
-      
-        return svg.node();
-      }
+        this.constroi()    
+    }
+
+    constroi() {
+        
     }
 }
